41ab99137546e013d26eaabca9bb61d1326d4d92
Abjerry97 <abj97x@gmail.com>
2023-11-25 18:30:32 +0100

    Add home section

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,43 +4,45 @@ import PageBtn from "../../components/PageBtn";
 import Modal from "../../components/Modal";
 import Registration from "../../components/Registration";
 import { usePageModal } from "../../context/PageModalProvider";
+import Confirmation from "../../components/Confirmation";
+import HomeDetails from "../../components/HomeDetails";
 
 export default function Home() {
-  const {toggleModalState} = usePageModal()
+  const { toggleModalState } = usePageModal();
   return (
     <div className="flex items-center justify-center">
-      <div
-        className="w-full md:w-8/12 lg:w-4/12 text-white   px-4 bg-landing-party h-screen bg-cover bg-center bg-no-repeat "
-        style={{
-          backgroundSize: "100% 100%",
-          // filter: "brightness(1.2) contrast(1.2)",
-        }}
-      >
-        {/* <div className="">
-        <div className="flex justify-between items-center">
-          <div className="text-xs">Back</div>
-          <div className="">Continental</div>
+      <div className="w-full md:w-8/12 lg:w-4/12">
+        <div
+          className="text-white px-4 bg-landing-party h-screen bg-cover bg-center bg-no-repeat "
+          style={{
+            backgroundSize: "100% 100%",
+            // filter: "brightness(1.2) contrast(1.2)",
+          }}
+        >
+          <Modal>
+            {/* <Registration/> */}
+            <Confirmation />
+          </Modal>
+          <button onClick={toggleModalState}> open modal</button>
         </div>
-        <div className="mt-3">
-          <h3 className=" font-semibold text-2xl">Remember <br/> these packages?</h3>
+        <div className="">
+          <div className=" px-4 mb-8">
+            <p className="mb-4">
+              Our eTravel Companion created these in real time based on the
+              requirements and interest you assigned to me, Lorem, Lorem, Lorem,
+              Lorem, Lorem, …
+            </p>
+
+            <PageBtn />
+
+            <PageBtn />
+          </div>
+
+          <HomeDetails/>
+       <HomeDetails/>
+       <HomeDetails/>
+       <div className=" my-4 text-center text-xs">Imprint | Data privacy</div>
         </div>
-      </div> */}
-        <Modal>
-      <Registration/>
-      </Modal>
-        {/* <div className="">
-        <div className=""> */}
-        {/* <LandingCard />
-          <LandingCard />
-          <LandingCard />
-          <LandingCard /> */}
-        {/* </div> */}
-        {/* <PageBtn />
-        <div className="text-center text-xs py-2"> Imprint | Data privacy</div> */}
-        {/* </div> */}
-
-
-        <button onClick={toggleModalState}> open modal</button>
       </div>
     </div>
   );
