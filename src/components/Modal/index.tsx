0b6ff642c1c80aed90971fe7be0bbfaf386d0a9f
Abjerry97 <abj97x@gmail.com>
2023-11-25 23:15:42 +0100

    fix modal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -5,7 +5,7 @@ export default function Modal(props:any) {
   return (
     <>
       {isOpen ? (
-        <div className="block m-auto items-center w-full md:w-6/12 lg:w-4/12 inset-0 h-screen  fixed bg-black ">
+        <div className="block m-auto items-center w-full md:w-6/12 lg:w-4/12 inset-0 max-h-screen  fixed bg-black ">
         <div className="h-[calc(100%-36px)] m-3 z-50 bg-white opacity-100 rounded-xl border-2 border-white overflow-hidden">
           <button
             className="p-1 ml-auto bg-transparent border-0 text-black float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
@@ -14,9 +14,8 @@ export default function Modal(props:any) {
             <span className="bg-transparent text-black h-6 w-6 text-2xl block outline-none focus:outline-none">
               ×
             </span>
-          </button>
-          <div className="">
-          {props.children}</div>
+          </button> 
+          {props.children}
         </div></div>
       ) : (
         <></>
